c8dac391e2c18f3301dd5d95b446f1cf481971bb
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-02 09:02:47 +0530

    feature: hero section and cards completed

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -7,34 +7,42 @@ const images = [
   "/images/hero-3.jpg",
   "/images/hero-2.jpg",
   "/images/hero-4.jpg",
-
 ];
 
 function Hero() {
-  const [imageIndex, setImageIndex] = useState(3);
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     setImageIndex((prev) => (prev === 2 ? 0 : prev + 1));
-  //   }, 5000);
-  //   return () => clearInterval(intervalId);
-  // }, []);
+  const [imageIndex, setImageIndex] = useState(1);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setImageIndex((prev) => (prev === 2 ? 0 : prev + 1));
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <div>
       <div
-      className="min-h-screen bg-no-repeat bg-center bg-cover "
-      style={{
-        backgroundImage: `url(${images[imageIndex]})`,
-      }}
-    >
-      <Navbar/>
-     <div className="flex flex-col items-center justify-center p-3 absolute top-1/3">
-     <h2 className="text-5xl font-bold text-gray-950 ">Cashews for Wealth,Health and for Prosperity</h2>
-        <p className="text-balance mt-4">Veniam Lorem commodo nulla dolore aliqua nostrud aliquip occaecat sint.Occaecat nulla commodo aliqua deserunt incididunt fugiat laboris mollit veniam ipsum.</p>
-     </div>
-    </div>
-    <div className="">
-      <HeroCards/>
-    </div>
+        className="min-h-screen bg-cover bg-center bg-no-repeat"
+        style={{
+          backgroundImage: `url(${images[imageIndex]})`,
+        }}
+      >
+        <Navbar />
+        <div className="text-center mx-auto flex flex-col items-center justify-center min-h-96">
+          <h2 className="text-5xl font-bold text-white max-w-3xl">
+            Cashews for Wealth,Health and for Prosperity
+          </h2>
+          <p className="text-balance mt-4  max-w-3xl font-bold text-slate-100">
+            Veniam Lorem commodo nulla dolore aliqua nostrud aliquip occaecat
+            sint.Occaecat nulla commodo
+          </p>
+          <div className="flex items-center gap-2 mt-4">
+            <button className="px-6 py-2 border bg-transparent bg-orange-500 hover:bg-transparent text-white hover:text-orange-500 border-orange-500 hover:border-orange-500 rounded-full">Get Started</button>
+            <button className="px-6 py-2 border bg-transparent hover:bg-blue-500 rounded-full border-blue-500 hover:border-blue-500 text-blue-500 hover:text-white">Learn More</button>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <HeroCards />
+      </div>
     </div>
   );
 }
