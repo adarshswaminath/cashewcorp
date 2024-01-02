dea5b8325d8636e241ad044890bcc1a4b15b8038
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-02 10:56:27 +0530

    feature: why choode us section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from "./Hero"
 import About from "./About"
 import Leaders from "./Leaders"
+import ChooseUs from "./ChooseUs"
 function Home() {
   return (
     <div>
         <Hero/>
         <About/>
         <Leaders/>
+        <ChooseUs/>
     </div>
   )
 }
