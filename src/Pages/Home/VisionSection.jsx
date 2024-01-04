0a103af686e31bbc164a744d413b4b3a9ac78f35
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-04 21:03:21 +0530

    refactor(VisionSection): typo correction

--- src/Pages/Home/VisionSection.jsx
+++ src/Pages/Home/VisionSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function VisionSection() {
   return (
     <div className="p-4 container mx-auto shadow-xl bg-white rounded-lg lg:p-8">
-      <h3 className="text-4xl font-bold uppercase mb-4">Vission</h3>
+      <h3 className="text-4xl font-bold uppercase mb-4">Vision</h3>
       <p className="text-justify text-gray-700">
         The Vision of KSCDC is to be a World Classs Organization ans a Leader in
         the Cashew Industry by providing 300 day of employment an year and
