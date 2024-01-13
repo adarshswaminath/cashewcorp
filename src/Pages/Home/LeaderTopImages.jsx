d01307bac55059a57d43c3784b027d4a197c2d77
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-13 14:35:59 +0530

    feature: new leaders section added

--- src/Pages/Home/LeaderTopImages.jsx
+++ src/Pages/Home/LeaderTopImages.jsx
@@ -2,15 +2,20 @@ import React from "react";
 
 function LeaderTopImages() {
   return (
-    <div className="mt-6 lg:mt-12 p-2 flex items-cemter justify-center gap-2">
-      <div className="grid place-items-center">
+    <div className="mt-6 lg:mt-12 p-2 items-cemter justify-evenly gap-2 lg:flex grid grid-cols-2">
+      <div className="grid place-items-center ">
+        <div>
         <img
           src="https://lc.kerala.gov.in/sites/default/files/styles/photo200x240/public/2021-08/cm.jpg?itok=5N70jXna"
-          className="object-cover h-60 rounded-xl"
+          className="object-cover h-60 rounded-xl lg:-mt-20"
           alt="Main"
         />
         <p className="text-center text-gray-900">Name Of Person</p>
-        <p className="text-center text-blue-500 font-bold">Chief Minister Of Kerala</p>
+        <p className="text-center text-blue-500 font-bold">
+        {" "}
+          Chief Minister Of Kerala
+        </p>
+        </div>
       </div>
       <div>
         <img
@@ -19,7 +24,41 @@ function LeaderTopImages() {
           alt=""
         />
         <p className="text-center text-gray-900">Name Of Person</p>
-        <p className="text-center text-blue-500 font-bold"> Minister Of Kerala</p>
+        <p className="text-center text-blue-500 font-bold">
+          {" "}
+          Minister Of Kerala
+        </p>
+      </div>
+      {/* central logo */}
+      <img src="/images/CDC.png" className="h-60" alt="CDC logo" />
+      {/* last card section */}
+      {/* chairman message */}
+      <div className="flex flex-col items-center card h-auto w-72 bg-gray-100">
+        <h3 className="font-bold">Chairman Message</h3>
+        <img
+          src="/images/chairman.png"
+          className="h-60 w-60 object-cover rounded-full"
+          alt="Chairman Image"
+        />
+        <p className="text-balance text-center">
+          Amet nostrud excepteur in labore ex adipisicing sunt consectetur irure
+          deserunt
+        </p>
+        <p className="mx-auto font-bold">Read More...</p>
+      </div>
+      {/* director message */}
+      <div className="flex flex-col items-center card h-auto bg-gray-100 w-72">
+        <h3 className="font-bold">Director Message</h3>
+        <img
+          src="/images/director.png"
+          className="h-60 w-60 object-cover rounded-full"
+          alt="Chairman Image"
+        />
+        <p className="text-balance text-center">
+          Amet nostrud excepteur in labore ex adipisicing sunt consectetur irure
+          deserunt.
+        </p>
+        <p className="mx-auto font-bold">Read More...</p>
       </div>
     </div>
   );
