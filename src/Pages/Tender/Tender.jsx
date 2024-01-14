9ca7bfb95a8ee15075bc560c8b0352c5e8240884
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-14 16:15:41 +0530

    feature multi page and routing added

---
+++ src/Pages/Tender/Tender.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../Components/Navbar"
+
+function Tender() {
+  return (
+    <div>
+        <Navbar/>
+        <div className="min-h-screen flex items-center justify-center">
+            Tender
+        </div>
+    </div>
+  )
+}
+
+export default Tender
