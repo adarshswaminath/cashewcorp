cff561e5c842228f098b19c6e5e44ae24788b900
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-20 10:16:25 +0530

    bug: need to refactor and re design product section

--- src/Pages/Home/Product.jsx
+++ src/Pages/Home/Product.jsx
@@ -1,72 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import ScrollAnimate from 'react-animate-on-scroll';
-import { Link } from 'react-router-dom';
-import { FaArrowRight,FaArrowLeft } from "react-icons/fa";
+import React from "react";
+import ScrollAnimate from "react-animate-on-scroll";
+import { Link } from "react-router-dom";
 
-import product1 from '/images/products/product-7.png';
-import product2 from '/images/products/product-8.png';
-import product3 from '/images/products/product-9.png';
-import product4 from '/images/products/product-10.png';
-import product5 from '/images/products/product-11.png';
-import product6 from '/images/products/product-12.png';
+import product1 from "/images/products/product-7.png";
+import product2 from "/images/products/product-8.png";
+import product3 from "/images/products/product-9.png";
+import product4 from "/images/products/product-10.png";
+import product5 from "/images/products/product-11.png";
+import product6 from "/images/products/product-12.png";
+import product8 from "/images/products/product-2.png";
+import product9 from "/images/products/product-3.png";
+import product10 from "/images/products/product-4.png";
+import product11 from "/images/products/product-5.png";
+import product12 from "/images/products/product-6.png";
 
-const productData = [product1, product2, product3, product4, product5, product6];
+const productData = [
+  product1,
+  product2,
+  product3,
+  product4,
+  product5,
+  product6,
+  product8,
+  product9,
+  product10,
+  product11,
+  product12,
+];
 
 const Product = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % productData.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + productData.length) % productData.length);
-  };
-
-  useEffect(() => {
-    const intervalId = setInterval(nextSlide, 2000); // Auto slide every 2000 milliseconds (2 seconds)
-
-    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
-  }, []); // Empty dependency array to run the effect only once on mount
-
   return (
     <div className="flex flex-col items-center">
       <ScrollAnimate animateIn="fadeIn">
-        <h2 className="font-semibold text-3xl text-red-500">Our Products</h2>
+        <h2 className="font-semibold text-3xl border-l-2 p-2 border-red text-orange-500">
+          Our Products
+        </h2>
       </ScrollAnimate>
-      <ScrollAnimate animateIn="fadeIn" delay={200}>
-        <div className="relative overflow-hidden h-48 w-48 lg:w-96 lg:h-96 mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 overflow-y-hidden h-56">
           {productData.map((product, index) => (
-            <img
+            <div
               key={index}
-              src={product}
-              alt={`Product ${index + 1}`}
-              className={`absolute top-0 left-0 w-full h-full transition-transform duration-500 ${
-                index === currentIndex ? 'opacity-100' : 'opacity-0'
-              } ${index === currentIndex ? '' : 'transform scale-90'}`}
-            />
+              className="relative overflow-hidden transition-transform transform duration-500 hover:scale-110 hover:z-10"
+            >
+              <img
+                src={product}
+                alt={`Product ${index + 1}`}
+                className="h-48 w-48 object-cover transition-transform transform scale-100"
+              />
+            </div>
           ))}
-        </div>
-        <div className="flex items-center justify-center">
-          <button
-            className="px-5 py-2 mr-2"
-            onClick={prevSlide}
-          >
-            <FaArrowLeft/>
-          </button>
-          <button
-            className="px-5 py-2 "
-            onClick={nextSlide}
-          >
-           <FaArrowRight/>
-          </button>
         </div>
         <div className="flex items-center justify-center mt-4">
           <button className="px-5 py-2 border border-black">
             <Link to="/product">View More</Link>
           </button>
         </div>
-      </ScrollAnimate>
+
     </div>
   );
 };
