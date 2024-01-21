2a35d1c03e1fd7544bab81eca9629c42b7445b6f
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-21 23:27:22 +0530

    style: gallery butn designed

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -47,10 +47,11 @@ function Gallery() {
         <div className="banner">
           <h3 className='text-3xl font-bold text-white'>Gallery</h3>
         </div>
-        <div className="grid grid-cols-3 lg:grid-cols-5 place-items-center justify-items-center gap-2 space-x-3 mt-4 mb-4">
+        <div className="grid grid-cols-3  gap-3 lg:flex items-center justify-center space-x-3 mt-4 mb-4">
           <button className={`px-5 py-1 border hover:bg-red-500 hover:text-white ${filter === 'all' && 'bg-red-500 text-white'}`} onClick={() => handleFilterChange('all')}>All</button>
           <button className={`px-5 py-1 border hover:bg-red-500 hover:text-white ${filter === 'functions' && 'bg-red-500 text-white'}`} onClick={() => handleFilterChange('functions')}>Functions</button>
           <button className={`px-5 py-1 border hover:bg-red-500 hover:text-white ${filter === 'product' && 'bg-red-500 text-white'}`} onClick={() => handleFilterChange('product')}>Product</button>
+          <button className={`px-5 py-1 border hover:bg-red-500 hover:text-white ${filter === 'achievements' && 'bg-red-500 text-white'}`} onClick={() => handleFilterChange('achievements')}>Achievements</button>
           <button className={`px-5 py-1 border hover:bg-red-500 hover:text-white ${filter === 'awards' && 'bg-red-500 text-white'}`} onClick={() => handleFilterChange('awards')}>Awards</button>
           <button className={`px-5 py-1 border hover:bg-red-500 hover:text-white ${filter === 'factories' && 'bg-red-500 text-white'}`} onClick={() => handleFilterChange('factories')}>Factories</button>
         </div>
