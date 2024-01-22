0d3a80e2aaf251a610e623dbbe3eb5156ffc8ef3
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-22 21:12:52 +0530

    feature: get app background image added

--- src/Pages/Home/PlayStore.jsx
+++ src/Pages/Home/PlayStore.jsx
@@ -1,10 +1,13 @@
 function PlayStore() {
   return (
-    <div className="bg-gradient-to-r from-[#ED213A] to-[#93291E] flex flex-col items-center justify-center">
+    <div
+      className="flex flex-col items-center justify-center"
+      style={{ backgroundImage: "url('/images/banner/app.jpg')", backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}
+    >
       <div className="lg:flex items-center justify-between grid ">
         {/* app layout */}
-        <div className="relative text-white text-4xl flex flex-col items-center justify-center">
-          <div className="absolute h-96 w-96 rounded-full bg-white shadow-lg"></div>
+        <div className="relative text-black text-4xl flex flex-col items-center justify-center">
+          <div className="absolute h-96 w-96 rounded-full bg-white shadow-xl"></div>
           <img
             data-aos="fade-up"
             data-aos-duration="500"
@@ -15,10 +18,10 @@ function PlayStore() {
         </div>
         {/* btns */}
         <div className="">
-          <h2 data-aos="zoom-in" className="text-3xl lg:text-5xl text-white font-bold text-center">
+          <h2 data-aos="zoom-in" className="text-3xl lg:text-5xl text-black font-bold text-center">
             Try Our KSCDC Mobile Apps
           </h2>
-          <p data-aos="zoom-in" className="max-w-xl text-center mt-4 text-white">
+          <p data-aos="zoom-in" className="max-w-xl text-center mt-4 text-black">
             Occaecat aute nisi non excepteur Lorem excepteur proident tempor et
             excepteur do deserunt.
           </p>
@@ -27,7 +30,7 @@ function PlayStore() {
               data-aos="zoom-out"
               src="/images/google-play.png"
               className="h-24"
-              alt="Playstrore image"
+              alt="Playstore image"
             />
             <img
               data-aos="zoom-out"
