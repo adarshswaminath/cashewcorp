e67207c79811499e0e92bf02e515627747462e2a
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-23 09:05:55 +0530

    feature: profile card re designed

--- src/Components/ProfileCard.jsx
+++ src/Components/ProfileCard.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import "./style/profilecard.css"
+import React, { useEffect } from 'react';
+import "./style/profilecard.css";
 
-function ProfileCard({name,image,position,content}) {
+function ProfileCard({ name, image, position, content,link }) {
+  
   return (
+    <section>
     <div className="container">
-           <div class="card">
-    <div class="face face1">
-        <div class="content">
-            <img src={image}/>
-            <h3>Design</h3>
+        <div className="card">
+            <div className="content">
+                <div className="imgBx">
+                    <img src={image}/>
+                </div>
+                <div className="contentBx">
+                    <h3>{name}<br/><span>{position}</span></h3>
+                </div>
+            </div>
+            <ul className="sci ">
+                <li className='bg-[#F4F5F6]'>
+                    <p>{content}</p>
+                </li>
+                <li className='bg-[#F4F5F6]'>
+                    {link ? (
+                        <button className='btn'>Read More</button>
+                    ) : <button></button>}
+                </li>
+            </ul>
         </div>
+      
+        
     </div>
-    <div class="face face2">
-        <div class="content">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                <a href="#">Read More</a>
-        </div>
-    </div>
-</div>
-    </div>
-
-  )
+</section>
+  );
 }
 
-export default ProfileCard
+export default ProfileCard;
