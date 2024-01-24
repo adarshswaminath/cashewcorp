15f8b6af9d22532a080d7c4ea6afcdd5ff56fe74
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-24 12:26:17 +0530

    refactor: banner images changed

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -5,7 +5,7 @@ function Banner({ image }) {
     <div className="p-3">
       <div className="lg:h-96 mb-6 overflow-hidden">
       <img
-        className="object-cover object-center w-full lg:h-full "
+        className="object-cover object-center w-full lg:h-full"
         src={`/images/banner/${image}`}
         alt="Banner"
       />
