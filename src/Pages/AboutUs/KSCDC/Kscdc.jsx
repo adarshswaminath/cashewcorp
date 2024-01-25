8e5d381df088d5d79beebc3063fbe177e80e40ef
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-25 10:43:36 +0530

    style: change the structure of about section

--- src/Pages/AboutUs/KSCDC/Kscdc.jsx
+++ src/Pages/AboutUs/KSCDC/Kscdc.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import DirectorBoard from '../../Home/DirectorBoard';
-import Navbar from '../../../Components/Navbar';
-import Footer from '../../../Components/Footer';
-import Banner from "../../../Components/Banner"
+import React from "react";
+import DirectorBoard from "../../Home/DirectorBoard";
+import Navbar from "../../../Components/Navbar";
+import Footer from "../../../Components/Footer";
+import Banner from "../../../Components/Banner";
 
 function Kscdc() {
   return (
     <div>
       <Navbar />
-      <Banner image="about.jpg"/>
+      <Banner image="about.jpg" />
       {/* Body content */}
-      <div className="container mx-auto p-6">
-        <section className="mb-8">
+      <div className="p-6">
+        <section data-aos="fade-up" className="mb-8">
           <h2 className="text-2xl font-semibold mb-4">ABOUT KSCDC</h2>
-          <p className="text-gray-700 leading-7">
+          <p  className="text-gray-700 leading-7">
             The Kerala State Cashew Development Corporation Limited (KSCDC) was
             incorporated in July 1969 and started commercial activities in the
-            year 1971 as a company fully owned by the Government of Kerala.
-            From the modest beginning in 1969, the Corporation has now grown
-            into a large organization playing a pivotal role in the cashew
-            industry.
+            year 1971 as a company fully owned by the Government of Kerala. From
+            the modest beginning in 1969, the Corporation has now grown into a
+            large organization playing a pivotal role in the cashew industry.
           </p>
 
           <p className="text-gray-700 leading-7 mt-4">
@@ -32,9 +31,9 @@ function Kscdc() {
 
           <p className="text-gray-700 leading-7 mt-4">
             KSCDC acts as a model employer in the field of the cashew industry
-            mainly to protect the interest of its workers and to provide
-            maximum employment to them, ensuring their statutory benefits like
-            minimum wages, bonus, etc., prevailing in the cashew industry.
+            mainly to protect the interest of its workers and to provide maximum
+            employment to them, ensuring their statutory benefits like minimum
+            wages, bonus, etc., prevailing in the cashew industry.
           </p>
 
           <p className="text-gray-700 leading-7 mt-4">
@@ -52,39 +51,58 @@ function Kscdc() {
             across 25 hectares in its facilities.
           </p>
         </section>
+        <div data-aos="zoom-in-down">
+          <img src="/images/company.jpeg" alt="Company Image" />
+        </div>
         <div>
-                <div className="grid lg:flex justify-center items-center gap-8">
-                  {/* Mission Card */}
-                  <div
-                    data-aos="fade-right"
-                    className="p-6 bg-white w-full lg:max-w-md flex flex-col items-start rounded-lg shadow-md border-l-4 border-[#FF1E1E] transform transition ease-in-out duration-300 hover:scale-105 hover:-translate-y-3"
-                  >
-                    <h2 className="text-3xl font-bold mb-4">Mission</h2>
-                    <p className="text-gray-700 leading-relaxed">
-                      KSDC aims at backward integration of its product line and
-                      has promoted Cashew Plantations in association with the
-                      Kerala State Agency for Cashew Cultivation. We have
-                      cultivated Cashew Plantation across 25 hectares in our
-                      facilities.
-                    </p>
-                  </div>
+          <div className="mt-8 grid lg:flex justify-center items-center gap-8">
+            {/* Mission Card */}
+            <div
+              data-aos="fade-right"
+              className="w-full "
+            >
+              <h2 className="text-3xl font-bold mb-4">Mission</h2>
+              <p className="text-gray-700 leading-relaxed">
+                KSDC aims at backward integration of its product line and has
+                promoted Cashew Plantations in association with the Kerala State
+                Agency for Cashew Cultivation. We have cultivated Cashew
+                Plantation across 25 hectares in our facilities.
+              </p>
+              <p className="text-gray-700 leading-relaxed">
+                This strategic collaboration underscores our commitment to
+                self-sufficiency and quality control. By cultivating cashew
+                trees, we not only secure a reliable raw material source but
+                also promote environmental sustainability and support local
+                farmers. This 25-hectare Cashew Plantation stands as a testament
+                to our dedication to responsible business practices and
+                community development.
+              </p>
+            </div>
 
-                  {/* Vision Card */}
-                  <div
-                    data-aos="fade-left"
-                    className="p-6 bg-white w-full lg:max-w-md flex flex-col items-start rounded-lg shadow-md border-l-4 border-[#FF1E1E] transform transition ease-in-out duration-300 hover:scale-105 hover:-translate-y-3"
-                  >
-                    <h2 className="text-3xl font-bold mb-4">Vision</h2>
-                    <p className="text-gray-700 leading-relaxed">
-                      The Vision of KSCDC is to be a World Class Organisation
-                      and a Leader in the Cashew Industry by providing 300 days
-                      of employment per year and achieve an Annual turnover of
-                      Rs. 1000 Crore by the year 2025.
-                    </p>
-                  </div>
-                </div>
-              </div>
-        <DirectorBoard />
+            {/* Vision Card */}
+            <div
+              data-aos="fade-left"
+              className="w-full"
+            >
+              <h2 className="text-3xl font-bold mb-4">Vision</h2>
+              <p className="text-gray-700 leading-relaxed">
+                The Vision of KSCDC is to be a World Class Organisation and a
+                Leader in the Cashew Industry by providing 300 days of
+                employment per year and achieve an Annual turnover of Rs. 1000
+                Crore by the year 2025.
+              </p>
+              <p className="text-gray-700 leading-relaxed">
+                This strategic collaboration underscores our commitment to
+                self-sufficiency and quality control. By cultivating cashew
+                trees, we not only secure a reliable raw material source but
+                also promote environmental sustainability and support local
+                farmers. This 25-hectare Cashew Plantation stands as a testament
+                to our dedication to responsible business practices and
+                community development.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
