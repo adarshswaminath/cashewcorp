40c81707d234d32158568972e585e1fbccba27b7
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-28 09:26:56 +0530

    feature: management page design updated

---
+++ src/Pages/AboutUs/Management/ManagementProfileCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function ManagementProfileCard({id, name, image, designation, contact }) {
+  return (
+    <div data-aos={`${ id%2 ===0 ? "fade-left" : "fade-right" }`} className="w-full rounded-lg overflow-hidden m-4 flex border p-4 hover:border-red-500 transform ease-in-out transition hover:-translate-y-3 hover:scale-100 duration-300 delay-100">
+      <img src={image} className="w-40 h-40 " alt="manager_image_profile" />
+      <div className="p-4">
+        <h1 className="text-xl font-bold text-red-500">{name}</h1>
+        <h3 className="text-sm text-gray-600">HOD ({designation})</h3>
+        <ul className="mt-3 space-y-2">
+          {contact.map((item, index) => (
+            <li key={index} className="text-gray-700">{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default ManagementProfileCard;
