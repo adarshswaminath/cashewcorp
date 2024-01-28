0d65d9dceb46b0e36a331c9b33859faf77487cf7
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-28 21:02:27 +0530

    feature: award section images caption added

--- src/Pages/Gallery/AwardImages.jsx
+++ src/Pages/Gallery/AwardImages.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import React from 'react';
+
+const awardData = [
+  {
+    id: 1,
+    imageSrc: '/images/gallery/awards/1.jpg',
+    title: 'Award Title 1',
+    description: 'Minim pariatur irure nostrud consequat nulla proident.',
+  },
+  {
+    id: 2,
+    imageSrc: '/images/gallery/awards/2.jpg',
+    title: 'Award Title 2',
+    description: 'Minim pariatur irure nostrud consequat nulla proident.',
+  },
+  {
+    id: 3,
+    imageSrc: '/images/gallery/awards/3.jpg',
+    title: 'Award Title 3',
+    description: 'Minim pariatur irure nostrud consequat nulla proident.',
+  },
+  {
+    id: 4,
+    imageSrc: '/images/gallery/awards/4.jpg',
+    title: 'Award Title 4',
+    description: 'Minim pariatur irure nostrud consequat nulla proident.',
+  },
+];
 
 function AwardImages() {
   return (
-    <div>
-        <div className="grid gap-4 grid-cols-1 lg:grid-cols-4">
-        {Array.from({length:4}).map((_,index) => (
-            <img src={`/images/gallery/awards/${index+1}.jpg`} alt="" />
-        ))}
+    <div  className="grid gap-4 grid-cols-1 lg:grid-cols-4 p-3">
+      {awardData.map((award) => (
+        <div data-aos="flip-up" key={award.id}>
+          <img src={award.imageSrc} alt={`Award ${award.id}`} />
+          <div>
+            <h3 className="text-lg font-bold">{award.title}</h3>
+            <p>{award.description}</p>
+          </div>
         </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default AwardImages
+export default AwardImages;
