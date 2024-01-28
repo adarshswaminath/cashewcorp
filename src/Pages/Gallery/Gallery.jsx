c46258c87bf32a9bb1b30f56844c707f8be16ca9
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-28 20:39:23 +0530

    feature: in gallery added media download secti on insted of factory image

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import Navbar from '../../Components/Navbar';
 import Footer from "../../Components/Footer";
-import ProductImages from './ProductImages';
 import AwardImages from './AwardImages';
 import AchievementsImages from './AchievementsImages';
 import FunctionImage from './FunctionImage'
 import Banner from "../../Components/Banner"
 import Videos from './Videos';
+import DownloadMedia from './DownloadMedia';
 
 
 function Gallery() {
@@ -23,8 +23,8 @@ function Gallery() {
         return <FunctionImage />;
       case 'awards':
         return <AwardImages />;
-      case 'factories':
-        return <FactoryImage />;
+      case 'downloads':
+        return <DownloadMedia/>;
       case 'achievements':
         return <AchievementsImages />;
       case 'video':
@@ -35,7 +35,6 @@ function Gallery() {
           <>
             <AwardImages />
             <FunctionImage />
-            <FactoryImage />
             <AchievementsImages />
             <Videos/>
           </>
@@ -48,13 +47,13 @@ function Gallery() {
       <Navbar />        
       <div className=" min-h-screen">
         <Banner image="gallery.jpg"/>
-        <div className="grid grid-cols-3  gap-3 lg:flex items-center justify-center space-x-3 mt-4 mb-4">
+        <div className="grid grid-cols-3  gap-3 lg:flex items-center justify-center space-x-3 mt-4 mb-4 p-3">
           <button className={`px-5 py-1 border hover:bg-[#FF1E1E] hover:text-white ${filter === 'all' && 'bg-[#FF1E1E] text-white'}`} onClick={() => handleFilterChange('all')}>All</button>
           <button className={`px-5 py-1 border hover:bg-[#FF1E1E] hover:text-white ${filter === 'functions' && 'bg-[#FF1E1E] text-white'}`} onClick={() => handleFilterChange('functions')}>Functions</button>
           <button className={`px-5 py-1 border hover:bg-[#FF1E1E] hover:text-white ${filter === 'video' && 'bg-[#FF1E1E] text-white'}`} onClick={() => handleFilterChange('video')}>Videos</button>
           <button className={`px-5 py-1 border hover:bg-[#FF1E1E] hover:text-white ${filter === 'achievements' && 'bg-[#FF1E1E] text-white'}`} onClick={() => handleFilterChange('achievements')}>Achievements</button>
           <button className={`px-5 py-1 border hover:bg-[#FF1E1E] hover:text-white ${filter === 'awards' && 'bg-[#FF1E1E] text-white'}`} onClick={() => handleFilterChange('awards')}>Awards</button>
-          <button className={`px-5 py-1 border hover:bg-[#FF1E1E] hover:text-white ${filter === 'factories' && 'bg-[#FF1E1E] text-white'}`} onClick={() => handleFilterChange('factories')}>Factories</button>
+          <button className={`px-5 py-1 border hover:bg-[#FF1E1E] hover:text-white ${filter === 'downloads' && 'bg-[#FF1E1E] text-white'}`} onClick={() => handleFilterChange('downloads')}>Downloads</button>
         </div>
         <div className="flex flex-col items-center">
           {renderImages()}
@@ -67,11 +66,4 @@ function Gallery() {
 
 export default Gallery;
 
-function FactoryImage(){
-  return(
-    <div>
-      <img src="/images/factory.jpeg" alt="Factory image" />
-    </div>
-  )
-}
 
