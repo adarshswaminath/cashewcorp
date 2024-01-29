8f08dd569af3d974f1d75926a2e57391c1195357
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-29 07:00:31 +0530

    feature: product filteration added

--- src/Pages/Products/ProductList.jsx
+++ src/Pages/Products/ProductList.jsx
@@ -1,33 +1,72 @@
-import React, { useState } from 'react';
-import ProductCard from './Components/ProductCard';
-import { productData } from './constant';
-import ProductModal from './Components/ProductModal';
+import React, { useState } from "react";
+import ProductCard from "./Components/ProductCard";
+import { productData } from "./constant";
+import ProductModal from "./Components/ProductModal";
 
 function ProductList() {
- 
-
   const [showModal, setShowModal] = useState(false);
   const [selectedData, setSelectedData] = useState({
-    id:0,
+    id: 0,
   });
+  const [filterType, setFilterType] = useState(null);
+
+  const filteredProductData = filterType
+    ? productData.filter((product) => product.type === filterType)
+    : productData;
+
+  const handleFilterChange = (type) => {
+    setFilterType(type);
+  };
 
   return (
-    <div className='min-h-screen p-3'>
-      
+    <div className="min-h-screen p-3">
+      <div className="flex justify-center mb-4 gap-2">
+      <button onClick={() => handleFilterChange(null)} 
+        className={`btn hover:bg-red-300 ${filterType === null && "bg-red-500 text-white btn-active"}`}>
+          View All
+        </button>
+        <button
+          onClick={() => handleFilterChange("RoastedAndSalt")}
+          className={` btn hover:bg-red-300 ${
+            filterType === "RoastedAndSalt" && "btn-active bg-red-500 text-white" 
+          }`}
+        >
+          Roasted and Salted
+        </button>
+        <button
+          onClick={() => handleFilterChange("ValueAddedProducts")}
+          className={`btn hover:bg-red-300 ${
+            filterType === "ValueAddedProducts"
+              && "btn-active bg-red-500 text-white"
+
+          }`}
+        >
+          Value Added Products
+        </button>
+        <button
+          onClick={() => handleFilterChange("GiftBoxPackets")}
+          className={`btn hover:bg-red-300 ${
+            filterType === "GiftBoxPackets" && "btn-active bg-red-500 text-white"
+          }`}
+        >
+          Gift Box Packets
+        </button>
+      </div>
+
       <div className="grid gap-4 grid-cols-2 lg:grid-cols-5 place-items-center justify-items-center">
-        {productData.map((product) => (
-          <ProductCard 
-            setSelectedData={setSelectedData} 
-            setShowModal={setShowModal} 
-            key={product.id} 
-            {...product} />
+        {filteredProductData.map((product) => (
+          <ProductCard
+            setSelectedData={setSelectedData}
+            setShowModal={setShowModal}
+            key={product.id}
+            {...product}
+          />
         ))}
       </div>
-      {showModal ? 
-        <ProductModal 
-          selectedData={selectedData} 
-          setShowModal={setShowModal} 
-        /> : null}
+
+      {showModal ? (
+        <ProductModal selectedData={selectedData} setShowModal={setShowModal} />
+      ) : null}
     </div>
   );
 }
--- src/Pages/Products/constant/index.js
+++ src/Pages/Products/constant/index.js
@@ -3,6 +3,8 @@ export const productData = [
     id:1,
     name: "Cashew Supreme",
     image: "/images/products/product-1.png",
+    second_image:"/images/products/product-1.png",
+    type: "RoastedAndSalt", 
     grade: '180',
     rating: 4,
     description: "A supreme blend of high-quality cashews for a delightful snacking experience.",
@@ -32,157 +34,68 @@ export const productData = [
   },
   {
     id:2,
-    name: "Cashew Apple Squash",
+    name: "Cashew Supreme",
     image: "/images/products/product-2.png",
-    grade: '210',
+    second_image:"/images/products/product-1.png",
+    type: "GiftBoxPackets", 
+    grade: '180',
     rating: 4,
-    description: "A delightful blend of the exotic cashew apple's natural sweetness and a burst of citrusy freshness",
+    description: "A supreme blend of high-quality cashews for a delightful snacking experience.",
     packet_weights: [
       {
         weight: "1kg",
-        mrp: 1395,
-        selling_rate: 977,
+        mrp: 1699,
+        selling_rate: 1190,
       },
       {
         weight: "500gm",
-        mrp: 704,
-        selling_rate: 492
+        mrp: 856,
+        selling_rate: 600
       },
       {
         weight: "250gm",
-        mrp: 357,
-        selling_rate: 250,
+        mrp: 433,
+        selling_rate: 303,
       },
+      {
+        weight: "100gm",
+        mrp: 180,
+        selling_rate: 126,
+      }
     ],
     percentage_discount: 30 
   },
   {
     id:3,
-    name: "Cashew Pine Jam",
+    name: "Cashew Supreme",
     image: "/images/products/product-3.png",
-    grade: '210',
+    second_image:"/images/products/product-1.png",
+    type: "ValueAddedProducts", 
+    grade: '180',
     rating: 4,
-    description: "A delightful blend of the exotic cashew apple's natural sweetness and a burst of citrusy freshness",
+    description: "A supreme blend of high-quality cashews for a delightful snacking experience.",
     packet_weights: [
       {
         weight: "1kg",
-        mrp: 1260,
-        selling_rate: 822,
+        mrp: 1699,
+        selling_rate: 1190,
       },
       {
         weight: "500gm",
-        mrp: 632,
-        selling_rate: 445
+        mrp: 856,
+        selling_rate: 600
       },
       {
         weight: "250gm",
-        mrp: 323,
-        selling_rate: 226,
+        mrp: 433,
+        selling_rate: 303,
       },
       {
         weight: "100gm",
-        mrp: 136,
-        selling_rate: 96,
+        mrp: 180,
+        selling_rate: 126,
       }
-
     ],
     percentage_discount: 30 
   },
-  {
-    id: 4,
-    name: "Roasted Cashew Mix",
-    image: "/images/products/product-4.png",
-    grade: '240',
-    rating: 4.5,
-    description: "A flavorful mix of roasted cashews, almonds, and pistachios for the ultimate snacking pleasure.",
-    packet_weights: [
-      {
-        weight: "1kg",
-        mrp: 1899,
-        selling_rate: 1499,
-      },
-      {
-        weight: "500gm",
-        mrp: 999,
-        selling_rate: 799
-      },
-      {
-        weight: "250gm",
-        mrp: 549,
-        selling_rate: 439,
-      },
-      {
-        weight: "100gm",
-        mrp: 249,
-        selling_rate: 199,
-      }
-    ],
-    percentage_discount: 20
-  },
-
-  // Product 5
-  {
-    id: 5,
-    name: "Cashew Chocolate Crunch",
-    image: "/images/products/product-8.png",
-    grade: '180',
-    rating: 4.2,
-    description: "Irresistible chocolate-coated cashews for a perfect blend of sweetness and crunchiness.",
-    packet_weights: [
-      {
-        weight: "1kg",
-        mrp: 1599,
-        selling_rate: 1299,
-      },
-      {
-        weight: "500gm",
-        mrp: 799,
-        selling_rate: 649
-      },
-      {
-        weight: "250gm",
-        mrp: 399,
-        selling_rate: 329,
-      },
-      {
-        weight: "100gm",
-        mrp: 179,
-        selling_rate: 149,
-      }
-    ],
-    percentage_discount: 15
-  },
-  {
-    id: 6,
-    name: "Spicy Masala Cashews",
-    image: "/images/products/product-6.png",
-    grade: '210',
-    rating: 4.3,
-    description: "Tantalize your taste buds with our perfectly spiced masala cashews, a spicy twist to your snacking routine.",
-    packet_weights: [
-      {
-        weight: "1kg",
-        mrp: 1799,
-        selling_rate: 1399,
-      },
-      {
-        weight: "500gm",
-        mrp: 899,
-        selling_rate: 699
-      },
-      {
-        weight: "250gm",
-        mrp: 469,
-        selling_rate: 349,
-      },
-      {
-        weight: "100gm",
-        mrp: 219,
-        selling_rate: 169,
-      }
-    ],
-    percentage_discount: 25
-  },
-
-
 ];
