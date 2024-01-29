88b2d59c42eb01abd7362f15e988df9ca745c59b
adarshswaminath <adarshswaminath7@gmail.com>
2024-01-29 09:44:36 +0530

    style: contact page post a query

--- src/Pages/Contact/PostAQuery.jsx
+++ src/Pages/Contact/PostAQuery.jsx
@@ -1,11 +1,85 @@
 import React from "react";
-import { FaPhoneAlt, FaEnvelope, FaBuilding } from 'react-icons/fa';
+import { FaPhoneAlt, FaEnvelope, FaBuilding } from "react-icons/fa";
+import { BiLogoTelegram } from "react-icons/bi";
 
 const PostAQuery = () => {
   return (
-    <div className="lg:flex  w-full">
-      <div className="w-full border border-error rounded-r-2xl flex flex-col items-center justify-center text-center lg:w-2/5 bg-white p-3 ">
-        {/* contact details */}
+    <div className="p-8 flex items-center justify-center">
+      <div className="grid lg:flex w-full">
+        <div className="w-full lg:w-2/5 bg-[#FF1E1E] text-white flex flex-col items-center text-center p-4">
+          <h2 className="text-4xl font-bold ">Contact Details</h2>
+          <hr className="border border-white w-full" />
+          <p className="text-2xl mt-2 mb-3 font-bold">
+            The Kerala State Cashew Development Corporation Limited
+          </p>
+          <div className="mt-6 mb-6 max-w-md">
+            <p className="text-xl">
+              (A Government of Kerala Undertaking), Cashew House, Mundakkal,
+              Kollam, Kerala, India - 691 001
+            </p>
+          </div>
+          <div className="mt-6 flex flex-col items-center text-lg">
+            <p className="flex gap-2 items-center">
+              <FaPhoneAlt /> +91 474 2742271, 2742172,2742273, 2742954
+            </p>
+            <p>Gram: 'KASUVANDI' </p>
+            <p className="flex gap-2 items-center">
+              {" "}
+              <FaEnvelope /> kscdc@kerala.gov.in
+            </p>
+            <p className="flex gap-2 items-center">
+              <FaEnvelope /> ho@cashewcorporation.com{" "}
+            </p>
+            <p className="mt-3">CIN:U15493KL1969SGC002234</p>
+          </div>
+        </div>
+        {/* form */}
+        <form className="w-full lg:w-3/5 bg-white flex flex-col items-center p-4">
+          <h2 className="font-bold">Send a Message</h2>
+          <div className="grid lg:flex gap-2 w-full">
+            <input
+              type="text"
+              placeholder="Name"
+              className="input bg-gray-200 w-full"
+            />
+            <input
+              type="tel"
+              placeholder="Phone No"
+              className="input bg-gray-200 w-full"
+            />
+          </div>
+          <textarea
+            placeholder="Address"
+            className="textarea bg-gray-200 w-full mt-2"
+            id=""
+            cols="20"
+            rows="2"
+          ></textarea>
+          <input
+            type="email"
+            placeholder="Email"
+            className="input mt-2 bg-gray-200 w-full"
+          />
+          <textarea
+            placeholder="Message"
+            className="textarea bg-gray-200 w-full mt-2"
+            id=""
+            cols="20"
+            rows="8"
+          ></textarea>
+          <button className="bg-[#FF1E1E] btn mt-2 hover:bg-red-400 text-white">
+            Send Message <BiLogoTelegram className="text-xl" />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default PostAQuery;
+{
+  /* <div className="w-full border border-error rounded-r-2xl flex flex-col items-center justify-center text-center lg:w-2/5 bg-white p-3 ">
+       
         <h2 className="text-4xl font-bold">Contact Details</h2>
         <p className="text-2xl mt-2 mb-3 font-bold">The Kerala State Cashew Development Corporation Limited</p>
         <hr className="border border-red-400 w-full"/>
@@ -16,7 +90,7 @@ const PostAQuery = () => {
           </p>
         </div>
         <hr className="border border-red-400 w-full"/>
-        {/* contact number */}
+    
         <div className="mt-6 flex flex-col items-center text-lg">
           <p className="flex gap-2 items-center">
           <FaPhoneAlt/> +91 474 2742271, 2742172,2742273, 2742954
@@ -25,23 +99,5 @@ const PostAQuery = () => {
           <p className="flex gap-2 items-center"> <FaEnvelope/> kscdc@kerala.gov.in  <FaEnvelope/> ho@cashewcorporation.com </p>
           <p>CIN:U15493KL1969SGC002234</p>
         </div>
-      </div>
-      {/* form */}
-      <div className="w-full lg:w-3/5 bg-white border border-error p-2 justify-center flex flex-col rounded-l-2xl">
-        <form className="p-6 mx-auto gap-2 bg-white rounded flex flex-col items-center justify-center">
-          <h2 className="font-bold text-xl">Submit Your Message</h2>
-          <div className="grid lg:flex items-center gap-2 w-full">
-            <input type="text" className="input bg-gray-200 w-full" placeholder="Name" />
-            <input type="tel" className="input bg-gray-200 w-full" placeholder="Phone" />
-          </div>
-          <input type="email" placeholder="Email" className="w-full input bg-gray-200" />
-          <textarea className="textarea bg-gray-200 w-full" cols="40" rows="5" placeholder="Address"></textarea>
-          <textarea className="textarea bg-gray-200 w-full" cols="40" rows="5" placeholder="Message"></textarea>
-          <button className="btn bg-red-400 hover:bg-red-300 text-white">Submit</button>
-        </form>
-      </div>
-    </div>
-  );
-};
-
-export default PostAQuery;
+      </div> */
+}
