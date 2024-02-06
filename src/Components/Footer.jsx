f2691b0099a955208bea5da041f31dac6ab72692
adarshswaminath <adarshswaminath7@gmail.com>
2024-02-06 07:25:02 +0530

    chore: footer content upgraded

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,46 +3,61 @@ import {
   LuFacebook,
   LuGithub,
   LuGitlab,
-  LuHammer,
   LuInstagram,
   LuLinkedin,
   LuTwitter,
 } from "react-icons/lu";
+
 let footerData = [
   {
     title: "Product",
-    links: [
-      "landing-page",
-      "popup-builder",
-      "web-design",
-      "content",
-      "integration",
+    values: [
+      { path: "/product",label: "Plain Cashews" },
+      { path: "/product",label: "Roasted And Salted"},
+      { path: "/product", label: "Value Added Products"},
+      { path: "/product",label: "Popular Products"},
+      { path: "/product",label: "Premeium Products"},
+      { path: "/product",label: "Online Shopping"},
+
     ],
   },
   {
-    title: "Use Cases",
-    links: ["web-design", "marketers", "small-business", "website-builder"],
-  },
-  {
-    title: "Resources",
-    links: ["academy", "blog", "themes", "hosting", "developers", "support"],
-  },
-  {
     title: "Company",
-    links: ["about-us", "career", "faqs", "teams", "contact-us"],
+    values: [
+      { path: '/kscdc', label: "KSCDC" },
+      { path: '/org',label: "Organization" },
+      { path: '/director-board', label: "Board Of Director"},
+      { path: '/management',label: "Management" },
+      { path: '/product-characteristics', label: "Product Characteristics" },
+      { path: '/franchisee', label: "Franchisee" },
+    ],
   },
+  {
+    title: "Sitemap",
+    values: [
+      { path: '/kscdc', label: "About Us" },
+      { path: '/product',label: "Product" },
+      { path: '/tender', label: "Tender"},
+      { path: '/career',label: "Career" },
+      { path: 'rti', label: "RTI" },
+      { path: '/gallery', label: "Gallery" },
+      { path: '/contact', label: "Contact" },
+    ],
+  },
+  
 ];
 
 const backgroundImage =
   "https://images.pexels.com/photos/1054218/pexels-photo-1054218.jpeg?auto=compress&cs=tinysrgb&w=600";
 
-function FooterLinks({ title, links }) {
+function FooterLinks({ title, values }) {
+  console.table(values);
   return (
     <div data-aos="fade-up" className="flex flex-col items-start gap-3">
       <h3 className="font-bold">{title}</h3>
-      {links.map((link, index) => (
-        <Link key={index} to="/" className="capitalize">
-          {link}
+      {values.map((value, index) => (
+        <Link key={value.path} to={value.path} className="capitalize">
+          {value.label}
         </Link>
       ))}
     </div>
@@ -62,14 +77,14 @@ function FooterSocialMediaPannel() {
       <div className="p-4 bg-white mt-3 text-black lg:w-3/4 rounded-lg">
         <h2 className="text-center font-extrabold tracking-wide">Follow us</h2>
         <div className="flex justify-evenly gap-3 m-2">
-          {socialMedis.map((icon) => (
+          {socialMedis.map((media) => (
             <a
               data-aos="flip-up"
-              href={icon.link}
-              key={icon}
+              href={media.link}
+              key={media.icon}
               className="text-xl transform transition ease-in-out duration-300 hover:scale-150 hover:-translate-y-2"
             >
-              {icon.icon}
+              {media.icon}
             </a>
           ))}
         </div>
@@ -80,8 +95,8 @@ function FooterSocialMediaPannel() {
 
 function FooterCopyRight() {
   return (
-    <div className="flex flex-col lg:flex-row justify-between text-gray-500 mt-2">
-      <span className="text-sm mb-2 lg:mb-0">@ 2023 All Rights Reserved</span>
+    <div className="flex flex-col lg:flex-row justify-between text-gray-500 ">
+      <span className="text-sm mb-2 lg:mb-0">@ 2024 All Rights Reserved</span>
       <div className="text-sm grid grid-cols-2 gap-2 lg:grid-cols-5 text-white">
         <span>Privacy Policy</span>
         <span>Terms of Use</span>
@@ -108,7 +123,7 @@ function Footer() {
           KSCDC
         </h2>
         {/* footer links */}
-        <div className="grid gap-3 grid-cols-2  lg:grid-cols-4 place-items-center ">
+        <div className="grid gap-3 lg:flex items-center justify-evenly mb-6">
           {footerData.map((data) => (
             <FooterLinks key={data.title} {...data} />
           ))}
@@ -138,23 +153,33 @@ function Footer() {
               alt="Footer image"
             />
             <div className="flex flex-col items-center">
-            <img
-              src="/images/footer5.png"
-              className="h-[75px] w-[75px] rounded-lg"
-              alt="Footer image"
-            />
-            <p className="text-xs font-bold">Time In Delivery</p>
+              <img
+                src="/images/footer5.png"
+                className="h-[75px] w-[75px] rounded-lg"
+                alt="Footer image"
+              />
+              <p className="text-xs font-bold">Time In Delivery</p>
             </div>
           </div>
         </div>
         <div className="flex items-center justify-center">
-        <div className="flex items-center justify-between w-3/4">
-          <img data-aos="zoom-out-right" src="/images/logo-2.png" className="object-contain h-72 w-72" alt="gov image" />
-          <img data-aos="zoom-out-left" src="/images/CDC.png" className="object-contain h-44 w-44" alt="gov image" />
+          <div className="flex items-center justify-between w-3/4">
+            <img
+              data-aos="zoom-out-right"
+              src="/images/logo-2.png"
+              className="object-contain h-72 w-72"
+              alt="gov image"
+            />
+            <img
+              data-aos="zoom-out-left"
+              src="/images/CDC.png"
+              className="object-contain h-44 w-44"
+              alt="gov image"
+            />
           </div>
         </div>
         {/* copy right section */}
-        <FooterCopyRight/>
+        <FooterCopyRight />
       </div>
     </div>
   );
