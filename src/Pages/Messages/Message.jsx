372a93f272f3ed1b0dcbecfb2aa1d2717e15920a
adarshswaminath <adarshswaminath7@gmail.com>
2024-02-07 12:34:36 +0530

    feature: message image rounded

--- src/Pages/Messages/Message.jsx
+++ src/Pages/Messages/Message.jsx
@@ -17,7 +17,7 @@ const leaderData = [
     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum inlabore laudantium deserunt fugiat numquam.",
     message: "Ex elit quis non qui tempor amet nisi laboris do cupidatat. Ea et excepteur consequat Lorem dolore occaecat velit in irure sunt do. Elit ea voluptate in et occaecat culpa officia et adipisicing excepteur mollit enim.",
     image:
-      "https://minister-industries.kerala.gov.in/wp-content/uploads/2021/09/prajeev.jpg",
+      "/images/rajeev.jpeg",
     title: "Hon. Minister's Message"
   },
   {
@@ -49,7 +49,7 @@ const Message = () => {
                 <img
                   src={leader.image}
                   alt={leader.name}
-                  className="w-60 h-60 object-cover mb-4 rounded-md"
+                  className="w-60 h-60 object-cover mb-4 rounded-b-full"
                 />
                <div>
                <h2 className="text-3xl font-bold mb-2">{leader.name}</h2>
