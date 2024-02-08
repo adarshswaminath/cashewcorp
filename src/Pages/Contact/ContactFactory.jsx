b1dc739c7665ea04634bde8cf9bb8f3836727750
adarshswaminath <adarshswaminath7@gmail.com>
2024-02-08 15:20:25 +0530

    change: accordion component in contact page

--- src/Pages/Contact/ContactFactory.jsx
+++ src/Pages/Contact/ContactFactory.jsx
@@ -4,7 +4,7 @@ import { factoriesData } from "../Org";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import Banner from '../../Components/Banner';
 import { ContactBox } from './ContactUs';
-
+import Accordion from '../../Components/Accordion';
 const data = [
   {
     name: "Shri. Sunil John K",
@@ -157,34 +157,6 @@ const ContactFactory = () => {
   )
 };
 
-
-// accordion 
- function Accordion({title,content}){
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleAccordion = () => {
-    setIsOpen(!isOpen);
-  };
-
-  return (
-    <div className="border  rounded-lg bg-[#0757A9]   mb-2">
-      <div
-        className="flex justify-between items-center p-4 cursor-pointer"
-        onClick={toggleAccordion}
-      >
-        <h3 className="text-lg font-semibold text-white">{title}</h3>
-        <span className={`transform text-white ${isOpen ? 'rotate-180' : 'rotate-0'} transition-transform`}>
-          &#9660;
-        </span>
-      </div>
-      {isOpen && (
-        <div className="p-4 bg-gray-100">
-          {content}
-        </div>
-      )}
-    </div>
-  );
-}
 
 
 
