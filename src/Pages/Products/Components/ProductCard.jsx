958a09f5c3c47028277a93b2e7d8a51aff86cb78
adarshswaminath <adarshswaminath7@gmail.com>
2024-02-08 14:18:00 +0530

    feature: buy now icon added

--- src/Pages/Products/Components/ProductCard.jsx
+++ src/Pages/Products/Components/ProductCard.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ProductModal from "./ProductModal";
+import { MdShoppingCart } from "react-icons/md";
 
 function ProductCard({
   setSelectedData,
@@ -76,9 +77,10 @@ function ProductCard({
         <div className="mt-4 flex items-center justify-end">
           <button
             onClick={handleUserClick}
-            className="text-white bg-red-600 hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 transition duration-300 ease-in-out"
+            className="text-white flex items-center gap-2  bg-red-600 hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 transition duration-300 ease-in-out"
           >
             Buy Now
+            <MdShoppingCart/>
           </button>
         </div>
       </div>
