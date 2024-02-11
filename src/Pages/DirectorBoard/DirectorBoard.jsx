61509c4853300ceed07da1e8cb302b230d5895db
adarshswaminath <adarshswaminath7@gmail.com>
2024-02-11 22:23:08 +0530

    feature: baord of director page translation added

--- src/Pages/DirectorBoard/DirectorBoard.jsx
+++ src/Pages/DirectorBoard/DirectorBoard.jsx
@@ -5,85 +5,17 @@ import Banner from "../../Components/Banner";
 import DirectorCard from "./DirectorCard";
 import TranslateButton from "../../Components/TranslateButton";
 import { useLanguage } from "../../contexts/LanguageContext";
-
-const DirectorsData = [
-  {
-    image: "/images/directors/chairman.png",
-    title: "Shri. S. Jayamohan",
-    caption: "Chairman",
-    email: "chairman@cashewcorporation.com",
-    phone: "+919447417711"
-  },
-  {
-    image: "/images/directors/dir-1.png",
-    title: "Shri. Sunil John .K",
-    caption: "Managing Director",
-    email: "md@cashewcorporation.com",    
-    phone: "+919847071750",
-  },
-  {
-    image: "/images/directors/dir-9.png",
-    title: "Shri. B. Pratheep Kumar",
-    caption: "Director",
-    email: "cmcashewcorporation@gmail.com",
-    phone: "",
-
-  },
-  {
-    image: "/images/directors/dir-10.png",
-    title: "Shri. Siju Jacob",
-    caption: "Director",
-    email: "cmcashewcorporation@gmail.com",
-    phone: "",
-
-  },
-  {
-    image: "/images/directors/dir-8.png",
-    title: "Dr. B S Suran",
-    caption: "Director",
-    email: "cmcashewcorporation@gmail.com",
-    phone: "",
-
-  },
-  {
-    image: "/images/directors/dir-4.png",
-    title: "Shri. B Sujeendran",
-    caption: "Director",
-    email: "cmcashewcorporation@gmail.com",
-    phone: "",
-
-  },
-  {
-    image: "/images/directors/dir-5.png",
-    title: "Shri. G. Babu",
-    caption: "Director",
-    email: "cmcashewcorporation@gmail.com",
-    phone: "",
-
-  },
-  {
-    image: "/images/directors/dir-7.png",
-    title: "Shri. Saji D. Anand",
-    caption: "Director",
-    email: "cmcashewcorporation@gmail.com",
-    phone: "",
-
-  },
-  {
-    image: "/images/directors/dir-6.png",
-    title: "Adv. Sooranadu S Sreekumar",
-    caption: "Director",
-    email: "cmcashewcorporation@gmail.com",
-    phone: "",
-
-  },
-];
+import enDirectorBoard from "./enDirectorBoard.json"
+import mlDirectorBoard from "./mlDirectorBoard.json"
+import useLanguageData from "../../Hook/useLanguageData";
 
 function DirectorBoardPage() {
   const {language,setLanguage} = useLanguage()
   const toggleLanguage = () => {
     setLanguage((prev) => !prev);
   };
+  const data = useLanguageData(enDirectorBoard,mlDirectorBoard)
+  console.table(data)
   return (
     <div>
       <Navbar />
@@ -91,9 +23,11 @@ function DirectorBoardPage() {
         <TranslateButton />
       </button>
       <Banner image="directorboard1.jpg" />
-      <h3 className="text-center text-3xl font-semibold text-red-400">Our Board Of Directors</h3>
+      <h3 className="text-center text-3xl font-semibold text-red-400">
+        { language ? "Board Of Directors" : "ഭരണസമിതി അംഗങ്ങൾ"}
+      </h3>
       <div className="grid gap-3 grid-cols-1 lg:grid-cols-2 place-items-center justify-items-center p-5">
-        {DirectorsData.map((data) => (
+        {data.map((data) => (
           <DirectorCard 
           title={data.title}
           caption={data.caption}
--- src/Pages/DirectorBoard/DirectorCard.jsx
+++ src/Pages/DirectorBoard/DirectorCard.jsx
@@ -1,7 +1,10 @@
 import { IoMdMail } from "react-icons/io";
 import { FaPhoneAlt } from "react-icons/fa";
+import { useLanguage } from "../../contexts/LanguageContext";
+
 
 function DirectorCard({ image, title, caption, email,phone }) {
+  const {language} = useLanguage()
   return (
     <div className="w-full  h-[260px] object-cover p-5 border bg-white hover:bg-red-300 group  flex items-center gap-x-8 hover:shadow-xl">
       <img src={image} className="h-48 w-44 rounded-b-full " alt="" />
@@ -10,7 +13,7 @@ function DirectorCard({ image, title, caption, email,phone }) {
           {title}
         </h3>
         <p className="font-bold">{caption}</p>
-        {title === "Shri. S. Jayamohan" || title === "Shri. Sunil John .K" ? (
+        {title === "Shri. S. Jayamohan" || title === "ശ്രീ. എസ്. ജയമോഹൻ" || title === "ശ്രീ. സുനിൽ ജോൺ .കെ" || title === "Shri. Sunil John .K" ? (
           <div>
             <a
               href={`mailto: ${email}`}
@@ -29,8 +32,8 @@ function DirectorCard({ image, title, caption, email,phone }) {
           </div>
         ) : (
           <div className=" gap-2">
-            <a  target="_blank" className="p-2 rounded-full shadow flex items-center gap-2 font-bold cursor-pointer"><IoMdMail /> Email</a>
-            <a  target="_blank" className="p-2 rounded-full shadow flex items-center gap-2 font-bold cursor-pointer"><FaPhoneAlt />Phone</a>
+            <a  target="_blank" className="p-2 rounded-full shadow flex items-center gap-2 font-bold cursor-pointer"><IoMdMail /> {language ? "Email" : "ഇമെയിൽ"}</a>
+            <a  target="_blank" className="p-2 rounded-full shadow flex items-center gap-2 font-bold cursor-pointer"><FaPhoneAlt />{language ? "Phone" : "ഫോൺ"}</a>
 
           </div>
         )}
