77691f34e66d0673216a38775ef6f592b59194c4
adarshswaminath <adarshswaminath7@gmail.com>
2024-02-15 09:16:06 +0530

    change: gallery achievements images gap added

--- src/Pages/Gallery/AchievementsImages.jsx
+++ src/Pages/Gallery/AchievementsImages.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function AchievementsImages() {
   return (
-    <div>
-        <div className="grid grid-cols-1 lg:grid-cols-5">
+    <div className='p-3'>
+        <div className="grid grid-cols-1 gap-3 place-content-center justify-items-center lg:grid-cols-3">
         {Array.from({length:5}).map((_,index) => (
             <img className='' src={`/images/gallery/achievments/${index+1}.png`} alt={`/images/products/product-${index+1}.png`} />
         ))}
