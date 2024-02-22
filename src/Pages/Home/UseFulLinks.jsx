278be6565d073a62c90b133e940383135d1a7140
adarshswaminath <adarshswaminath7@gmail.com>
2024-02-22 15:23:59 +0530

    change: usefull link key fro mapping chnaged

--- src/Pages/Home/UseFulLinks.jsx
+++ src/Pages/Home/UseFulLinks.jsx
@@ -46,7 +46,7 @@ function UseFulLinks() {
         <div className="marquee">
           <ul className="flex gap-3">
             {response.useful_links.map((value, index) => (
-                <Card key={value.id} link={value.name} />
+                <Card key={value.index} link={value.name} />
             ))}
           </ul>
         </div>
